d0894e3e94361ff5e5cdf4c6b489344aeb447d02
jovalmangalan <joval.mangalan@ontariotechu.net>
2025-11-05 20:53:05 -0500

    Set up structure and configuration

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+// Home page route
+router.get('/', (req, res) => {
+  res.render('index', { title: 'Home' });
+});
+
+// About page
+router.get('/about', (req, res) => {
+  res.render('about', { title: 'About Me' });
+});
+
+// Projects page
+router.get('/projects', (req, res) => {
+  res.render('projects', { title: 'Projects' });
+});
+
+// Contact page
+router.get('/contact', (req, res) => {
+  res.render('contact', { title: 'Contact' });
+});
+
+module.exports = router; 
